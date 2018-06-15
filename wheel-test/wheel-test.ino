841ae28909c8781d8e6d571eaa204806d703c10d
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2018-06-15 13:28:30 +0200

    Simplify whele test

--- wheel-test/wheel-test.ino
+++ wheel-test/wheel-test.ino
@@ -4,14 +4,22 @@
 #include <PinChangeIntConfig.h>
 
 
-irqISR(irq1,isr1);
-MotorWheel wheel1(9,8,6,7,&irq1);
+/* irqISR(irq1,isr1); */
+/* MotorWheel wheel1(9,8,6,7,&irq1); */
+
+/* irqISR(irq2,isr2); */
+/* MotorWheel wheel2(10,11,12,13,&irq2); */
+
+irqISR(irq3,isr3);
+MotorWheel wheel1(3,2,4,5,&irq3);
 
 
 void setup() {
-  TCCR1B=TCCR1B&0xf8|0x01;
+  TCCR1B=TCCR1B&0xf8|0x01;    // Pin9,Pin10 PWM 31250Hz
+  TCCR2B=TCCR2B&0xf8|0x01;    // Pin3,Pin11 PWM 31250Hz
   wheel1.PIDEnable(0.26, 0.02, 0, 10);
 }
+
 void loop() {
   wheel1.setSpeedMMPS(40, DIR_ADVANCE);
 
